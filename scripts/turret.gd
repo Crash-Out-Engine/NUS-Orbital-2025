1891320ce127b99197db45a18ff02e3c17a5f97d
Cxyu629 <chowxingyu123@gmail.com>
2025-04-21 01:50:46 +0800

    feat: performance enhancements

--- scripts/turret.gd
+++ scripts/turret.gd
@@ -4,11 +4,5 @@ extends StaticBody2D
 
 var target_provider: TargetProvider = null
 
-func _ready() -> void:
-	ranged.active = true
-	ranged.effect_mods.append_array([
-		HealthEffect.create_instant(1.0),
-	])
-
 func set_collidable(value: bool) -> void:
 	$CollisionShape2D.set_deferred("disabled", not value)
