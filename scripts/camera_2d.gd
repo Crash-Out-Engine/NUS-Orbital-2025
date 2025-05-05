c0875d63a79a07874d7671110079550055989865
Cxyu629 <chowxingyu123@gmail.com>
2025-05-05 20:07:25 +0800

    feat: clamp zoom

--- scripts/camera_2d.gd
+++ scripts/camera_2d.gd
@@ -4,13 +4,15 @@ const ZOOM_FACTOR: float = sqrt(2.0)
 
 const SMOOTHING_FACTOR: float = 0.9
 
+var _zoom_power: float = 0.0
 var _zooming: float = 0.0
 
 func _physics_process(_delta: float) -> void:
-	if Input.is_action_just_released("zoom in"):
+	if _zoom_power + _zooming < 1.5 and Input.is_action_just_released("zoom in"):
 		_zooming += 1.0
-	if Input.is_action_just_released("zoom out"):
+	if -6.5 < _zoom_power + _zooming and Input.is_action_just_released("zoom out"):
 		_zooming -= 1.0
 		
-	zoom *= ZOOM_FACTOR ** (lerpf(0, _zooming, 1.0 - SMOOTHING_FACTOR))
+	_zoom_power += lerpf(0, _zooming, 1.0 - SMOOTHING_FACTOR)
 	_zooming = lerpf(0, _zooming, SMOOTHING_FACTOR)
+	zoom = Vector2.ONE * ZOOM_FACTOR ** _zoom_power
