624e590f199376e843e228f38ced1635f9279840
Cxyu629 <chowxingyu123@gmail.com>
2025-05-08 15:29:19 +0800

    feat: improve loot movement with distance-based acceleration

--- scripts/loot.gd
+++ scripts/loot.gd
@@ -1,12 +1,16 @@
 class_name Loot extends Area2D
 
-const SPEED = 100
+const SPEED_CAPACITY = 100
 
 @onready var player: Player = $/root/Game/EntityContainer/Player
 
+var _velocity := Vector2.ZERO
+
 func _physics_process(delta: float) -> void:
 	var direction = global_position.angle_to_point(player.global_position)
-	global_position += Vector2.from_angle(direction) * SPEED * delta
+	var acceleration := 10000000.0 / global_position.distance_squared_to(player.global_position)
+	_velocity = Vector2.from_angle(direction) * (acceleration * delta + _velocity.length())
+	global_position += _velocity * delta
 	global_rotation = direction
 
 func _on_body_entered(body: Node2D) -> void:
