7544f9f394e7ae6909f40a447c0bea2d0dadef05
deltaMinor <73937826+deltaMinor@users.noreply.github.com>
2025-05-15 20:48:35 +0800

    refactor: reformat ui spritesheet to save space
    -change health bar to be handled by a center container

--- scripts/health_bar_container.gd
+++ scripts/health_bar_container.gd
@@ -1,13 +1,13 @@
-extends TextureProgressBar
+extends CenterContainer
 
 @export var player: Player
 @onready var label: Label = $Label
+@onready var health_bar: TextureProgressBar = $HealthBar
 
 func _ready() -> void:
 	player.health_changed.connect(update)
-	label.set_position(position + size/2 - label.size/2)
 	label.text = str(player.get_health())
 	
 func update(new_ratio: float) -> void:
-	value = 100 * new_ratio
+	health_bar.value = 100 * new_ratio
 	label.text = str(player.get_health())
