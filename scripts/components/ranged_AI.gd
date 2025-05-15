63589b1ff25ced3c50a82ada94332983c9b1d03b
deltaMinor <73937826+deltaMinor@users.noreply.github.com>
2025-05-15 12:17:53 +0800

    fix: fix turrets not playing idle animation

--- scripts/components/ranged_AI.gd
+++ scripts/components/ranged_AI.gd
@@ -13,8 +13,13 @@ func _ready() -> void:
 	if(has_gun_sound):
 		bullet_spawned.connect(play_fire)
 
+func play_idle() -> void:
+	if(has_gun_anim):
+		$GunSprite.play("idle")
+
 func _physics_process(_delta: float) -> void:
 	if !active or !ranged_cooldown.can_ranged():
+		play_idle()
 		return
 	
 	var team = ""
@@ -23,6 +28,7 @@ func _physics_process(_delta: float) -> void:
 	var target_provider := load("res://resources/target_provider.tres") as TargetProvider
 	var target = target_provider.get_target(get_parent().global_position, team)
 	if target == null:
+		play_idle()
 		return
 	if(has_gun_anim):
 		look_at(target.global_position)
