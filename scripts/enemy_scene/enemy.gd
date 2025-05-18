5e96205744a95bf695e7fa7e4f56d4aeafbd2a47
Cxyu629 <chowxingyu123@gmail.com>
2025-05-18 14:08:22 +0800

    refactor: remove unused variables/constants/nodes in Enemy scene

--- scripts/enemy_scene/enemy.gd
+++ scripts/enemy_scene/enemy.gd
@@ -3,16 +3,10 @@ extends RigidBody2D
 
 signal vfx_emitted(Node2D)
 
-const BLEED_TIME = 0.04
-const V_MODULATE = 100000000
-
-@export var target_provider := load("res://resources/target_provider.tres") as TargetProvider
-
 var loot_scene = preload("res://scenes/loot.tscn")
 
 @onready var timer: Timer = $Timer
 @onready var ranged: RangedAI = $Ranged
-@onready var target_priority: TargetPriority = $TargetPriority
 
 
 func _ready() -> void:
