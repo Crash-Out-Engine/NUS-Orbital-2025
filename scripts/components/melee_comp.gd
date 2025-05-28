4ea57d858367e4f8ff5bb32039aa3111505a08e1
Cxyu629 <chowxingyu123@gmail.com>
2025-05-28 20:33:53 +0800

    fix: correct Player's melee targets
    - Additionally change hurtboxes' colors and ordering for debugging purposes.
    - Additionally fix melee only triggering on a single entity.

--- scripts/components/melee_comp.gd
+++ scripts/components/melee_comp.gd
@@ -10,11 +10,12 @@ signal executed(entity: Node2D)
 
 
 func _physics_process(_delta: float) -> void:
-	for body in get_overlapping_bodies():
-		if (body != null
-				and body.get_node_or_null(^"Components/HitboxComp") != null
-				and body.get_node(^"Components/HitboxComp").is_targeted_by(target_filter)
-				and melee_cooldown.try_melee()):
-			for effect in effects:
-				body.get_node_or_null(^"Components/HitboxComp").trigger(effect, $"../../")
-			executed.emit(body)
+	if melee_cooldown.can_melee():
+		for body in get_overlapping_bodies():
+			if (body != null
+					and body.get_node_or_null(^"Components/HitboxComp") != null
+					and body.get_node(^"Components/HitboxComp").is_targeted_by(target_filter)):
+				melee_cooldown.do_melee()
+				for effect in effects:
+					body.get_node_or_null(^"Components/HitboxComp").trigger(effect, $"../../")
+				executed.emit(body)
