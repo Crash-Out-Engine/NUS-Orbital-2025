30d3f5469d0728c10729f079c29dee2e539a62e4
Cxyu629 <chowxingyu123@gmail.com>
2025-06-01 05:33:30 +0800

    ci: try fixing GUT ci

--- tests/integration/test_player_movement.gd
+++ tests/integration/test_player_movement.gd
@@ -1,9 +1,7 @@
 extends GutTest
 
-const _PLAYER_SCENE = preload("res://scenes/player.tscn")
-
 func test_move_forward():
-	var player := _PLAYER_SCENE.instantiate() as Player
+	var player := load("res://scenes/player.tscn").instantiate() as Player
 	assert_true(player != null)
 	
 	add_child_autofree(player)
