c911e2512c308041f02d76fe081285806ed71dbd
Cxyu629 <chowxingyu123@gmail.com>
2025-06-01 04:11:50 +0800

    ci: try fixing GUT ci

--- tests/integration/test_player_controls.gd
+++ tests/integration/test_player_controls.gd
@@ -1,16 +1,18 @@
 extends GutTest
 
+class TestMovement:
+	extends GutTest
 
-func test_movement():
-	var player := preload("res://scenes/player.tscn").instantiate() as Player
-	assert_true(player != null)
-	
-	add_child_autofree(player)
-	
-	var curr_position = player.position
-	Input.action_press("right")
-	await wait_frames(2)
-	Input.action_release("right")
-	var next_position = player.position
-	assert_eq(next_position.y, curr_position.y)
-	assert_gt(next_position.x, curr_position.x)
+	func test_movement():
+		var player := preload("res://scenes/player.tscn").instantiate() as Player
+		assert_true(player != null)
+		
+		add_child_autofree(player)
+		
+		var curr_position = player.position
+		Input.action_press("right")
+		await wait_frames(2)
+		Input.action_release("right")
+		var next_position = player.position
+		assert_eq(next_position.y, curr_position.y)
+		assert_gt(next_position.x, curr_position.x)
