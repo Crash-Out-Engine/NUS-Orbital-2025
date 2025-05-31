9ad463605b2f100452ea74f4677fd7cbfe89fecc
Cxyu629 <chowxingyu123@gmail.com>
2025-06-01 05:05:10 +0800

    ci: try fixing GUT ci

--- tests/integration/test_player_inputs.gd
+++ tests/integration/test_player_inputs.gd
@@ -1,5 +1,12 @@
 extends GutTest
 
+var _sender = InputSender.new(Input)
+
+
+func after_each():
+	_sender.release_all()
+	_sender.clear()
+
 
 func test_movement():
 	var player := preload("res://scenes/player.tscn").instantiate() as Player
@@ -7,10 +14,13 @@ func test_movement():
 	
 	add_child_autofree(player)
 	
+	
 	var curr_position = player.position
-	Input.action_press("right")
+	_sender.action_down("right").wait_frames(1)
+	await _sender.idle
+	
 	await wait_frames(2)
-	Input.action_release("right")
+	_sender.action_up("right")
 	var next_position = player.position
 	assert_eq(next_position.y, curr_position.y)
 	assert_gt(next_position.x, curr_position.x)
