5adaeba9ec22c870ad13b2fd9dbe40bcd434e7b5
Cxyu629 <chowxingyu123@gmail.com>
2025-06-01 05:24:56 +0800

    ci: try fixing GUT ci

--- tests/integration/test_player_controls.gd
+++ tests/integration/test_player_controls.gd
@@ -5,7 +5,9 @@ func test_move_forward():
 	assert_true(player != null)
 	
 	var curr_position = player.position
+	Input.action_press("up")
 	await wait_frames(2)
+	Input.action_release("up")
 	var new_position = player.position
 	assert_eq(curr_position.x, new_position.x)
 	assert_gt(curr_position.y, new_position.y)
