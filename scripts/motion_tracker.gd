5579445337a036a60e928a08874dbc5813220a82
Cxyu629 <chowxingyu123@gmail.com>
2025-06-02 18:05:21 +0800

    feat: implement `MotionTracker` utility and basic ranged predictive aiming

---
+++ scripts/motion_tracker.gd
@@ -0,0 +1,37 @@
+class_name MotionTracker
+extends Node
+## Utility node for tracking [class Node2D]'s motion.
+##
+## Since this class's initializer adds itself as a child to the tracked entity
+## and uses the entity's [member Node.process_physics_priority], it will run
+## after the entity's motion, ensuring the least latency.
+
+var position: Vector2
+var velocity: Vector2
+var acceleration: Vector2
+var _entity: Node2D
+
+
+## Attaches a [class MotionTracker] node to an entity.
+static func attach_to(entity: Node2D) -> MotionTracker:
+	return new(entity)
+
+
+func _init(entity: Node2D) -> void:
+	process_physics_priority = entity.process_physics_priority
+	entity.add_child(self)
+	_entity = entity
+
+
+func _ready() -> void:
+	self.name = "MotionTracker"
+
+
+func _physics_process(delta: float) -> void:
+	if _entity != null:
+		var prev_position = position
+		var prev_velocity = velocity
+
+		position = _entity.global_position
+		velocity = (position - prev_position) / delta
+		acceleration = (velocity - prev_velocity) / delta
