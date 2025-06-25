6586e8190cdbe2e1d8e38b598e392c21edbeb526
Cxyu629 <chowxingyu123@gmail.com>
2025-06-25 21:33:38 +0800

    feat: implement save/load scene for Explosion

--- scripts/explosion.gd
+++ scripts/explosion.gd
@@ -12,13 +12,16 @@ var effects: Array[Effect] = []
 @onready var anim_player = $AnimationPlayer as AnimationPlayer
 
 func _ready() -> void:
-	explosion_mod_comp._setup_mods()
+	explosion_mod_comp.setup_mods()
 
 func _enter_tree() -> void:
 	call_deferred("explode")
 
 
 func explode() -> void:
+	if not is_inside_tree():
+		return
+
 	for i in repeat_prop.value:
 		anim_player.speed_scale = 0.2 / timeout_prop.value
 		anim_player.play("explode")
@@ -29,11 +32,27 @@ func explode() -> void:
 func assign_mods(mods: Array[ModBase]) -> void:
 	explosion_mod_comp.mods.assign(mods)
 
-func set_size(size: float):
-	scale = size * Vector2(1.0, 1.0)
-
-
 func _on_body_entered(body: Node2D) -> void:
 	if (body.get_node_or_null(^"Components/HitboxComp") != null
 			and body.get_node(^"Components/HitboxComp").is_targeted_by(target_filter)):
 		body.get_node_or_null(^"Components/HitboxComp").trigger(effects, self)
+
+
+#region Save/load
+
+func save_scene() -> PackedByteArray:
+	var dict = {}
+	dict["position"] = position
+	dict["rotation"] = rotation
+	dict["target_filter"] = target_filter.save()
+	dict["effects"] = Effect.save_array(effects)
+	return var_to_bytes(dict)
+
+func load_saved_scene(data: PackedByteArray) -> void:
+	var dict = bytes_to_var(data)
+	position = dict["position"]
+	rotation = dict["rotation"]
+	target_filter = TargetFilter.from_saved(dict["target_filter"])
+	effects = Effect.from_saved_array(dict["effects"])
+
+#endregion
