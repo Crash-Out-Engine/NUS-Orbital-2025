0335780b65b3757ead92a7bdf85fbdbd5cdd79c5
Cxyu629 <chowxingyu123@gmail.com>
2025-06-28 22:01:18 +0800

    test: modify test to work with new input handling

--- tests/integration/test_player_controls.gd
+++ tests/integration/test_player_controls.gd
@@ -3,15 +3,15 @@ extends GutTest
 const _PLAYER_SCENE = preload("res://scenes/player.tscn")
 
 func test_move_forward():
-	var player := _PLAYER_SCENE.instantiate() as Player
+	var player = add_child_autofree(_PLAYER_SCENE.instantiate())
 	assert_true(player != null)
-
-	add_child_autofree(player)
+	
+	var sender := GutInputSender.new(player)
+	sender.add_receiver(Input)
 
 	var curr_position = player.position
-	Input.action_press("up")
-	await wait_frames(2)
-	Input.action_release("up")
+	sender.key_down(KEY_D).action_down("right").hold_for(1).wait("10f")
+	await sender.idle
 	var new_position = player.position
-	assert_eq(curr_position.x, new_position.x)
-	assert_gt(curr_position.y, new_position.y)
+	assert_lt(curr_position.x, new_position.x)
+	assert_eq(curr_position.y, new_position.y)
