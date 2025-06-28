2af730cc65f96512e15a07cf662b67b783b3d082
Cxyu629 <chowxingyu123@gmail.com>
2025-06-29 00:50:15 +0800

    feat: add auto-resize flag to SizeProp

--- scripts/properties/size_prop.gd
+++ scripts/properties/size_prop.gd
@@ -2,19 +2,31 @@ class_name SizeProp
 extends PropertyBase
 
 const MIN_SIZE: float = 0.1
-const GROWTH_SPEED: float = 1.0
 
 @export var _size: float = 1.0
+## If set to true, SizeProp will automatically resize entity's children when changed.
+@export var _auto: bool = true
 
 @onready var _entity = $"../.." as Node2D
 
 func _ready() -> void:
-	changed.connect(_handle_size_changed)
 	min_value = MIN_SIZE
 	value = _size
+	if _auto:
+		changed.connect(change_entity_size)
 
 
-func _handle_size_changed(_from, to: float) -> void:
+func change_entity_size(from: float, to: float, duration: float = 0.0) -> void:
+	var time_passed := 0.0
+
+	while time_passed < duration:
+		await get_tree().process_frame
+		time_passed += get_process_delta_time()
+		for child in _entity.get_children():
+			if child is Node2D:
+				child.scale = (
+						lerp(from, to * Vector2.ONE, clampf(time_passed / duration, 0.0, 1.0)))
+
 	for child in _entity.get_children():
 		if child is Node2D:
 			child.scale = to * Vector2.ONE
