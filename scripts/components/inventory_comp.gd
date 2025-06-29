9a29b94878b3547fb92850402e74a3edbaee39d2
deltaMinor <73937826+deltaMinor@users.noreply.github.com>
2025-06-29 20:35:14 +0800

    style: clear lint

--- scripts/components/inventory_comp.gd
+++ scripts/components/inventory_comp.gd
@@ -15,8 +15,6 @@ signal slots_updated(size: int, capacity: int)
 @export var mod_targeting_comp: ModTargetingComp
 @export var blueprint_comp: BlueprintComp
 
-@onready var player := get_parent().get_parent() as Player
-
 var inventory_ui: Control
 var _entity: Node2D
 var _entity_slot: ModSlotComp
@@ -27,6 +25,7 @@ var _scraps: int:
 		_scraps = value
 		scraps_changed.emit(prev_value, _scraps)
 
+@onready var player := get_parent().get_parent() as Player
 
 func _ready() -> void:
 	for mod in initial_mods:
