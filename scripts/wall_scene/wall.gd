01c72607f660d024a6efe82a1d0c541463a2e938
Cxyu629 <chowxingyu123@gmail.com>
2025-07-09 16:17:28 +0800

    feat: add `LootCrate` and `Wall` structures
    Note: requires further polishing.

---
+++ scripts/wall_scene/wall.gd
@@ -0,0 +1,32 @@
+class_name Wall
+extends StaticBody2D
+
+@export_group("Properties")
+@export var health: HealthProp
+
+
+func _ready() -> void:
+	health.emptied.connect(_die)
+
+
+func _die() -> void:
+	get_parent().server_remove_entity(self)
+
+
+#region Save/load
+
+func save_scene() -> PackedByteArray:
+	var dict := {}
+	dict["global_position"] = global_position
+	for property_node: PropertyBase in $Properties.get_children():
+		dict[property_node.name] = property_node.save()
+	return var_to_bytes(dict)
+
+
+func load_saved_scene(data: PackedByteArray) -> void:
+	var dict = bytes_to_var(data) as Dictionary	
+	global_position = dict["global_position"]
+	for property_node: PropertyBase in $Properties.get_children():
+		property_node.load_saved(dict[property_node.name])
+
+#endregion
